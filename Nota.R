a9a9dfeac10b59f5a00336c231582235c4708d60
Mugika <61477854+mugika19191@users.noreply.github.com>
2023-12-12 16:05:43 +0100

    Add files via upload

--- Nota.R
+++ Nota.R
@@ -6,8 +6,8 @@ Notak
 attach(Notak)
 
 #Begiratu lista bakoitzean dauden datuak EzZenbaki kantitatea kontuan artu gabe
-length(Notak1P[!is.na(Notak1P)])
-length(Notak2P[!is.na(Notak2P)])
+NP1=length(Notak1P[!is.na(Notak1P)])
+NP2=length(Notak2P[!is.na(Notak2P)])
 
 #Limiteak jarri
 heniaP1=max(Notak1P,na.rm=T)-min(Notak1P, na.rm = T)
@@ -15,8 +15,9 @@ heniaP1
 heniaP2=max(Notak2P,na.rm=T)-min(Notak2P,na.rm=T)
 heniaP2
 
-sqrt(length(Notak1P))#biak berdiank dira
-
+sqrt(NP1)
+sqrt(NP2)
+#7-ra urbilketa
 heniaP1/7
 heniaP2/7
 
@@ -147,18 +148,12 @@ IQR(Notak2P, type=2, na.rm = T)
 
 var = var(Notak1P, na.rm = T)
 
-N = length(Notak1P)
-N
-
-bar = var*(N-1)/N
+bar = var*(NP1-1)/NP1
 bar
 #BariantzaP2
 var2 = var(Notak2P, na.rm = T)
 
-N2 = length(Notak2P)
-N2
-
-bar2 = var2*(N2-1)/N2
+bar2 = var2*(NP2-1)/NP2
 bar2
 
 #Desbiderazio tipikoa
@@ -201,11 +196,11 @@ skewness(Notak2P, na.rm = T)
 
 #Pearson alborapen koefizientea
 
-Pearson = (bb-1)/des.tip
+Pearson = 3*(mean(Notak1P,na.rm=T)-median(Notak1P,na.rm=T))/des.tip
 Pearson
 
 #Pearson alborapen koefizienteaP2
-Pearson2 = (bb2-1)/des.tip2
+Pearson2 = 3*(mean(Notak2P,na.rm=T)-median(Notak2P,na.rm=T))/des.tip2
 Pearson2
 
 #Kurtosia
@@ -250,5 +245,82 @@ sd(Notak1P, na.rm = T)
 
 sd(Notak2P, na.rm = T)
 
-###########################################
+#
+#
+#KONFIANTZA TARTEAK Notak1P laginarekin
+
+#%99-ko konfiantza mailaz µ-rako konfiantza tartea, suposatuz desbiderazio tipikoa 1 dela (σ ezaguna izanda)
+KT99P1=c(mean(Notak1P, na.rm = T)-qnorm(0.995)*1/sqrt(NP1),mean(Notak1P, na.rm = T)+qnorm(0.995)*1/sqrt(NP1))
+KT99P1
+
+#%98-ko konfiantza mailaz µ-rako konfiantza tartea (σ ezezaguna izanda)
+KT98P1=c(mean(Notak1P, na.rm = T)-qt(0.01,NP1-1,lower.tail = F)*sd(Notak1P, na.rm = T)/sqrt(NP1),mean(Notak1P, na.rm = T)+qt(0.01,NP1-1,lower.tail = F)*sd(Notak1P, na.rm = T)/sqrt(NP1))
+KT98P1
+#beste modu bat
+t.test(Notak1P, na.rm = T, conf.level = 0.98)$conf
+
+#%97-ko konfiantza mailaz σ-rako konfiantza tartea, suposatuz batazbestekoa 5.5  dela (µ ezaguna)
+difP1=Notak1P-5.5
+difP1
+KT97P1=c(sum(difP1^2,na.rm = T)/qchisq(0.985,NP1),sum(difP1^2,na.rm = T)/qchisq(0.015,NP1))
+KT97P1
+
+#%95-ko konfiantza mailaz σ-rako konfiantza tartea, suposatuz µ ezezaguna dela
+KT95P1=c((NP1-1)*var(Notak1P, na.rm = T)/qchisq(0.975,NP1-1),(NP1-1)*var(Notak1P, na.rm = T)/qchisq(0.025,NP1-1))
+KT95P1
+
+#
+#
+#Bariantzen zatidura eta batazbestekoen kenduraren tarte estimazioak
+#Bariantzen zatidura
+#%95-eko konfiantza mailaz
+var.test(Notak1P,Notak2P)$conf#%emaitzak barne 1 balioa daukalez, berdinak direra esan dezakegu
+ 
+
+#Batazbestekoen kendura
+#%95-eko konfiantza mailaz
+t.test(Notak1P,Notak2P,var.equal = T)$conf#lehenengo partzialeko notak bb txikiagoa da
+
+#
+#
+###Hipotesi kontrastea
+#Populazioaren batazbestekorako
+#%5-eko adierazgarritasun mailaz, mu=5.5 dela Ho, eta deseberdina dela Ha.
+t.test(Notak1P, na.rm = T,mu=5.5)
+
+#%3-eko adierazgarritasun mailaz, bb 6 gehienez
+t.test(Notak1P, na.rm = T,conf.level=0.97,mu=6,alternative = "greater")
+
+#%7-eko adierazgarritasun mailaz, populazioaren batezbestekoa 4 gutxienez
+t.test(Notak1P, na.rm = T,conf.level=0.93,mu=4,alternative = "less")
+
+#Populazioaren bariantzarako
+#%5-eko adierazgarritasun mailaz,9-ko bariantza duen populaziotik datozela onartu daitekeen kontrastatu (µ ezezaguna)
+c(qchisq(0.025,NP1-1),qchisq(0.975,NP1-1))
+(NP1-1)*var(Notak1P,na.rm=T)/9
+
+#%3-ko adierazgarritasun mailaz, bariantza gehinez 10 dela (µ ezezaguna).
+#onarpen eremua
+c(0,qchisq(0.97,NP1-1))
+#eskualde kritikoa
+c(qchisq(0.97,NP1-1),Inf)
+#estatistikoa
+(NP1-1)*var(Notak1P,na.rm=T)/10
+
+#%7-ko adierazgarritasun mailaz, bariantza gutxienez 8 dela (µ ezezaguna).
+#onarpen eremua
+c(qchisq(0.07,NP1-1),Inf)
+#eskualde kritikoa
+c(0,qchisq(0.07,NP1-1))
+#estatistikoa
+(NP1-1)*var(Notak1P,na.rm=T)/8
+
+#Populazioen arteko hipotesi kontrastea
+#%5eko adierazgarritasun mailaz, esan al daiteke bi hirietako tenperaturen desbiderazio tipikoak berdinak direla?
+var.test(Notak1P,Notak2P)
+
+#%1eko adierazgarritasun mailaz, esan al daiteke bien batezbestekoak berdinak direla?
+t.test(Notak1P, Notak2P, conf.level = 0.99)
+
+
 
